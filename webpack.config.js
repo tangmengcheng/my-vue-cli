a37060e8ac703916dae5b5b693c678455e409b71
tangmengcheng <1018663766@qq.com>
2019-07-05 16:43:42 +0800

    add font/img... module

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development', // 配置开发环境模式
@@ -6,6 +8,11 @@ module.exports = {
     output: { // 打包文件的出口
         filename: 'bound.js', // 打包文件名
         path: path.resolve(__dirname, 'dist') // 打包文件存放的目录
+    },
+    devServer: {
+        hot: true, // 热更新
+        port: 3000,
+        contentBase: './dist'
     },
     module: { // 模块
         rules: [ // 规则
@@ -37,5 +44,13 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', // 生成的html文件名
+            template: './src/index.html' // 以那个文件作为模块
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
